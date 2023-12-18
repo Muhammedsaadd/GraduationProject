2eeea5cdd2c1a0650d384689e1c74b697d43151d
Muhammedsaadd <20195444@fue.edu.eg>
2023-12-18 05:09:31 +0200

    workout plan
    created a function that call the workouts from database and save the name of workouts in variable workoutname

--- GP/lib/workoutPlan.dart
+++ GP/lib/workoutPlan.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -12,8 +15,142 @@ class MyApp extends StatefulWidget{
   State<MyApp> createState() => _MyAppState();
 }
 class _MyAppState extends State<MyApp> {
+  var data;
+  var widg = [];
+  var workoutname = [];
+  Future getUserData() async{
+    var url =  Uri.parse(
+        'https://unmingled-dates.000webhostapp.com/getWorkout.php');
+
+    var response = await http.post(url, body: {
+      // "username": mobile,
+    }
+    );
+
+    // print(json.decode(response.body));
+    var data1 = await json.decode(response.body);
+    getData(data1);
+    print("data1 ${data1}");
+    data=data1;
+
+    print("mamaos ${data1.runtimeType}");
+    // Navigator.push(
+    //     context,
+    //     MaterialPageRoute(
+    //         builder: (context) =>
+    //             MainMenu(data)
+    //     ));
+    return data1;
+
+    // return json.decode(response.body);
+  }
+  getData(var data)  {
+    // Email =[] ;
+    // rate  =[];
+    // fullName =[];
+    // mobileNumber =[];
+    // gender =[];
+    // dateOfBirth=[] ;
+    print("data23 $data");
+    var fname;
+    var lname;
+    for(int i =0; i<data.length;i++) {
+      // Email.add(data[i]["Email"]);
+      // fname = data[i]["Fname"];
+      // lname = data[i]["Lname"];
+      // fullName .add(fname + " " + lname);
+      // // mobileNumber.add(data[i]["Email"]);
+      workoutname.add(data[i]["WorkoutName"]);
+      // rate.add(data[i]["Rate"]);
+    }
+     print("Workout Name $workoutname");
+    // print("rate $rate");
+    // print("full $fullName");
+    // print("date $dateOfBirth");
+    for(int i =0; i<data.length;i++){
+
+      // widg.add(
+      //   Container(
+      //       width: 392.5,
+      //       height: 500,
+      //       // decoration: BoxDecoration(
+      //       //     borderRadius: BorderRadius.only(topLeft: Radius.circular(150),topRight: Radius.circular(150)),
+      //       //     color: Colors.green,
+      //       //     image: DecorationImage(image: AssetImage("images/images.jpeg"), fit: BoxFit.fill)
+      //       // ),
+      //       child: Column(
+      //         children: [
+      //           Center(
+      //             child:  Container(
+      //               width: 300,
+      //               height: 350,
+      //               decoration: BoxDecoration(
+      //                   borderRadius: BorderRadius.only(topLeft: Radius.circular(150),topRight: Radius.circular(150)),
+      //                   color: Colors.yellowAccent,
+      //                   image: DecorationImage(image: AssetImage("images/images.jpeg"), fit: BoxFit.fill)
+      //               ),
+      //             ),
+      //
+      //           ) ,
+      //
+      //           Text("Coach ${fullName[i]}",style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),),
+      //           Container(
+      //               margin: EdgeInsets.symmetric(vertical: 12),
+      //               width: 100,
+      //               child: ClipRRect(
+      //                 borderRadius: BorderRadius.circular(29),
+      //                 child: SizedBox(height: 40, child:  ElevatedButton(
+      //                   // padding: EdgeInsets.symmetric(vertical: 18,horizontal: 38),
+      //                   // color: Colors.deepOrange,
+      //                   onPressed: () async {
+      //                     getUserData();
+      //                   },
+      //                   child: Text(
+      //                     'Info',
+      //                     style:
+      //                     TextStyle(color: Colors.black, fontSize: 18),
+      //                   ),
+      //                   style: ElevatedButton.styleFrom(
+      //                     primary: Colors.yellow,
+      //                   ),
+      //                 )),
+      //               )),
+      //           Row(
+      //             children: [
+      //               SizedBox(width: 135,),
+      //
+      //               Container(
+      //                 // width: 392.5,
+      //
+      //                 child: Row(
+      //                   children: [
+      //
+      //
+      //                     Icon(Icons.star,color: Colors.yellow,),
+      //                     Icon(Icons.star,color: Colors.yellow,),
+      //                     Icon(Icons.star,color: Colors.yellow,),
+      //                     Icon(Icons.star,color: Colors.yellow,),
+      //                     Icon(Icons.star,color: Colors.yellow,),
+      //                   ],
+      //                 ),
+      //
+      //               )
+      //             ],
+      //           )],
+      //       )
+      //
+      //
+      //   ),
+
+      //);
+
+    }
+    //print("wid ${widg.length}");
+
+  }
   @override
   Widget build(BuildContext context) {
+    getUserData();
     return MaterialApp(
         home: Scaffold(
         backgroundColor: Colors.black,
